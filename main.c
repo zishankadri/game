41b3b83d54867baefc60bddd5df3131805bedc4c
c0mplex <sadasipa@gmail.com>
2026-01-14 10:17:27 -0500

    adding window

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <raylib.h>
+
+const int WINDOW_HEIGHT;
+const int WINDOW_WIDTH;
+const char * title = "simple raylib game";
+
+int main(void)
+{
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, title);
+
+  SetTargetFPS(60);
+  while(!WindowShouldClose())
+  {
+    BeginDrawing();
+        ClearBackground(RAYWHITE);
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
+}
